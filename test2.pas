93588d0b5271bdb0402262c2ae6d03b2a8171cf1
kukorra <154669365+kukorra@users.noreply.github.com>
2025-06-16 02:34:54 +0500

    Update test2.pas

--- test2.pas
+++ test2.pas
@@ -4,8 +4,14 @@ var
 begin
   case ch of
     'a': writeln('A');
-    k: writeln('K');
-    i: writeln('I');
-    'b'...'d': writeln('B-D');
+    'k: writeln('K');
+    'i: writeln('I');
+    'b'...'d': writeln('B-D'); 
+    else
+      writeln('Другая буква');  
+      writeln('Неизвестно');   
   end;
+
+  if ch in ['a'..'z'] 
+    writeln('Это буква');
 end.
