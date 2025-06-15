fe95388ec8f6ab5471c1303257f11a85dcfce50f
kukorra <154669365+kukorra@users.noreply.github.com>
2025-06-16 02:34:36 +0500

    Update test1.pas

--- test1.pas
+++ test1.pas
@@ -1,7 +1,12 @@
 program Test1;
 var
-  i: integer;
+  i: integer;//adadasda
 begin
-  for i := 1 to 10...20 do
-    x = 5;
+  for i := 1 to 10...20   
+    x := 5;  
+    writeln(i);{asdada
+  if i > 5  
+    writeln('Больше 5');'asdsadsd
+  else
+    writeln('Меньше или равно 5');
 end.
