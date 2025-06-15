b0ca2f60bc1edde3511295f7260c4f011e8605f4
kukorra <154669365+kukorra@users.noreply.github.com>
2025-06-16 02:35:23 +0500

    Update test4.pas

--- test4.pas
+++ test4.pas
@@ -2,12 +2,15 @@ program Test4;
 var
   x, y: integer;
 begin
-  x = 5;
-  y = x...10;
-  case x of
+  x = 5;  
+  y := x...10;  
+
+  case x 
     1: y := 1;
-    2...3: y := 2;
-    4...5...6: y := 3;
-    k: y := 4;
+    2...3: y := 2;  
+    4...5...6: y := 3;  
+    k: y := 4;  
   end;
+
+  if x > 0 y := 1 else if x < 0 y := -1; 
 end.
